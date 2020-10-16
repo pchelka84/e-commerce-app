3caf859d4dc3a314e4b96b7a9edd798a3fd9162c
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-10-16 13:29:22 -0400

    Style titles

--- src/components/menuItem/menuItem.component.jsx
+++ src/components/menuItem/menuItem.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./menuItem.styles.scss";
+
 const MenuItem = ({ title, imageUrl, size }) => (
   <div
     style={{
@@ -9,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     className={`${size} menu-item`}
   >
     <div className="content">
-      <h1 className="titile">{title}</h1>
+      <h1 className="titile">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
