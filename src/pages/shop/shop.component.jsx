33a870b2af60468824e04a5dae3b22036d0f0cff
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-01-23 10:01:02 -0500

    Display four preview items in the shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
+import CollectionPreview from '../../components/preview-collection/collection-preview.component'
+
 class ShopPage extends Component {
    state = {
      collections: SHOP_DATA,
    }
 
    render() {
+     const { collections } = this.state;
      return (
-       <div>SHOP PAGE</div>
+       <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))
+        }
+       </div>
      )
    }
 }
