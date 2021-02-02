5d5605a2f445f0451c0937db88edebf24484ec6e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-01 19:52:06 -0500

    Fixing typo

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -27,7 +27,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className='sign-in'>
-        <h2>I alreadyhave an account</h2>
+        <h2>I already have an account</h2>
         <span class="title">Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
