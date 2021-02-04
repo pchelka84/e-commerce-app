d7b81b6abcebc53fc1898d34ec8c055184320f72
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-04 13:59:42 -0500

    Adding Sign in/out navigation

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
   render() {
     return ( 
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch> 
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
