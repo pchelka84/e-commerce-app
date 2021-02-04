34382777af7b776e2ec4371bf78353d6eab5c599
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-04 14:40:05 -0500

    Style sign-in buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -46,9 +46,11 @@ class SignIn extends Component {
             value={this.state.email} 
             handleChange={this.handleChange}
             label="password"
-            required /> 
-          <CustomButton type='submit'> Sign in </CustomButton> 
-          <CustomButton onClick={ signInWithGoogle } isGoogleSignIn> Sign in with Google </CustomButton>
+            required />  
+          <div className="buttons">
+            <CustomButton type='submit'> Sign in </CustomButton> 
+            <CustomButton onClick={ signInWithGoogle } isGoogleSignIn> Sign in with Google </CustomButton>
+          </div>
         </form>
       </div>
     );
