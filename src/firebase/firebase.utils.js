e9a08c735544be99d24a8cf938e724e0b53581e2
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-05 17:01:52 -0500

    Creating new users in db based user data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,39 @@ const config ={
   measurementId: "G-HEKH99GZGF"
 };
 
-firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  // console.log(snapShot);
+
+  // checks if the snapShot exists at this place
+  if (!snapShot.exists) {
+    const  { dispalyName, email } = userAuth;
+    const createdAt = new Date();
+
+    // Create snapShot at the current place if it doesn't exists
+    try {
+      await userRef.set({
+        dispalyName, 
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+}
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(config);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
