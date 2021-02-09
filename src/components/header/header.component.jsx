dd94d4f8195047e761fdbedb51000942559c5263
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-09 16:13:22 -0500

    Implement Redux in Cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import './header.style.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logocontainer' to="/">
       <Logo className='log' />
@@ -29,12 +29,13 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
-    <CartDropdown/>
+    { hidden ? null :  <CartDropdown/> }
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser: currentUser,
+  hidden: hidden
 })
 
 export default connect(mapStateToProps)(Header);
