163d175a0e3892703de7fb1237b6e0e62dac1c08
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-12 16:44:16 -0500

    Adjust line-height

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,14 +8,17 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss'; 
 
-
 const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {
-        cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
-      }
-    </div>
+  <div className="cart-dropdown">  
+        <div className="cart-items">
+          {cartItems.length ? (
+            cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            ) : 
+            (
+              <span className="empty-message">Your cart is empty</span>
+            )
+          }
+        </div>  
     <CustomButton>CHECKOUT</CustomButton>
   </div>
 );
