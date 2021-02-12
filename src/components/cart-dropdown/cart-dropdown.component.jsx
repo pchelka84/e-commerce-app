5339c2334d6840370e141a2cb8921eba5d19184b
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-12 16:48:33 -0500

    Adding Routing the Checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from './../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-import './cart-dropdown.styles.scss'; 
+import './cart-dropdown.styles.scss';  
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className="cart-dropdown">  
         <div className="cart-items">
           {cartItems.length ? (
@@ -19,7 +20,7 @@ const CartDropdown = ({ cartItems }) => (
             )
           }
         </div>  
-    <CustomButton>CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>CHECKOUT</CustomButton>
   </div>
 );
 
@@ -27,4 +28,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 })
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
