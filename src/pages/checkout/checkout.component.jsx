aaa5992bc8c28eee1b015d551ee22fa33c3b3de5
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-14 16:07:00 -0500

    Calculate and display total price with Redux and selectors

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react'; 
+ 
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'; 
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header"> 
       <div className="header-block">
@@ -20,7 +26,20 @@ const CheckoutPage = () => (
         <span>Remove</span>
       </div>
     </div>
+    {
+      cartItems.map(cartItem =>
+        cartItem.name)
+    }
+  
+    <div className="total">
+      <span>TOTAL: ${total}</span>
+    </div>
   </div>
-)
 
-export default CheckoutPage;
+);
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+})
+
+export default connect(mapStateToProps)(CheckoutPage);
