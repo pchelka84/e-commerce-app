5241c14e2fee2bb497d12ce92c6981fb616a9349
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-19 12:03:32 -0500

    Fix typos importing Collection Selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { selectCollections } from './../../redux/shop/shop.selectors';
+import { selectCollection } from './../../redux/shop/shop.selectors';
+import CollectionItem from './../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const CollectionPage = () => (
+
+const CollectionPage = ({ collection }) =>{ 
+  const { title, items } = collection;
+  return (
   <div className='collection-page'>
-  <h2>Collection Page</h2></div> 
-);
+    <h2 className='title'>{ title }</h2>
+    {
+      items.map(item => <CollectionItem key={item.id} item={item} />)
+    }
+  </div> 
+)};
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollections(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 
 export default connect(mapStateToProps)(CollectionPage);
