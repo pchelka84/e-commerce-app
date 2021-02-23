50eff0b38ee9cbea4f934ae438c32af5ea95718d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-23 15:52:39 -0500

    Pull shop data to the app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -46,12 +46,26 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   const collectionRef = firestore.collection(collectionKey); 
 
   const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
+  objectsToAdd.forEach((obj) => {
     const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
 
   return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+  console.log(transformedCollection);
 }
 
 if (firebase.apps.length === 0) {
