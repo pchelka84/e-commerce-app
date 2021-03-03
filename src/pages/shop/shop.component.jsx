bb92c2ee7939ea7a6bd8f0d3fe1ec806a9df6eab
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-03 11:02:27 -0500

    Fixing en error with initial collection mount and didMount async fetching

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ import CollectionsOverview from './../../components/collections-overview/collect
 import CollectionPage from '../collection/collection.component';
  
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectisCollectionLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.components'; 
 
@@ -21,19 +21,20 @@ class ShopPage extends Component {
   }
 
   render() { 
-    const { match, isCollectionFetching } = this.props; 
+    const { match, isCollectionFetching, isCollectionLoaded } = this.props; 
 
      return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} /> }/>
-        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>} />
+        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props}/>} />
       </div>
     );
   }
 } 
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionLoaded: selectisCollectionLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
