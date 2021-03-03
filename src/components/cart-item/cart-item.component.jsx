7e95229a7f29936f972906d8ec1153e66412a331
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-02 19:58:49 -0500

    Convert cart-item styling over to styled-components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import { CartItemContainer, CartItemImg, ItemDetails, ItemName} from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity} }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt="item"/>
-    <div className="item-details">
-      <span className="name">{name}</span>
-      <span className="price">{quantity} x ${price}</span>
+  <CartItemContainer>
+    <CartItemImg  src={imageUrl} alt="item"/>
+    <ItemDetails>
+      <ItemName>{name}</ItemName>
+      <span>{quantity} x ${price}</span>
 
-    </div>
-  </div>
+    </ItemDetails>
+  </CartItemContainer>
 );
 
-export default CartItem;
+export default CartItem
