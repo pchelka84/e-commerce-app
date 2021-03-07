aecac06a37193b0ac8ee3955a1ef09caae7c3167
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-07 09:39:14 -0500

    Fixing clear item from cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -24,10 +24,10 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
     case CartActionTypes.CLEAR_ITEM_FROM_CART: 
-    return {
-      ...state,
-      CartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
-    }
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+      }
     default:
       return state
   }
